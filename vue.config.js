f3f3d5bb186b3410681113287575b022b9bb75f3
Kamruzzaman <zaman7u@gmail.com>
2021-07-14 12:54:39 +0600

    Add vue.config.js file

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	chainWebpack: (config) => {
+		config.plugins.delete("prefetch");
+	},
+	devServer: {
+		https: false,
+	},
+	configureWebpack: {
+		module: {
+			rules: [{
+				test: /\.html$/,
+				loader: "vue-template-loader",
+				exclude: /index.html/,
+			}],
+		},
+	},
+};
